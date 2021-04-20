4f4b8624b75f67e7032c269d3140865e35c4c1fb
Veronica Alcala <veronica.alcala@gmail.com>
2021-04-20 12:03:40 +0200

    designed recipe card

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -4,13 +4,17 @@ import './Recipes.css'
 
 function RecipeCard({ recipe, details }) {
     return (
+        <>
         <section className='recipe-card' key={recipe.id}>
-            <img src={recipe.imgURL} className='recipe-list-img' alt='recipe'/>
+            <div className='img-card-container'>
+                <img src={recipe.imgURL} className='recipe-list-img' alt='recipe'/>
+                <p className='details-btn' onClick={details}>SEE RECIPE</p>
+            </div>
             <div className='recipe-card-title'>
                 <h5>{recipe.name}</h5>
             </div>
-            <button className='details-btn' onClick={details}>Details</button>
         </section>
+        </>
     )
 }
 
